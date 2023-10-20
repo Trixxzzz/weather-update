4586762f16bbf5592da4efad51946f63d17c04c3
Trixxzzz <ramtekearyan2003@gmail.com>
2023-10-20 20:07:46 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "ec479c1569msh7e7a4b75b2cd644p177a70jsndce06a28125e",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "622c79bc37cc2d1659c26e788ade7ecb"; // enter your key from openweather API
